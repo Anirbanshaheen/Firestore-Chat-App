f975198dea3832d3b09ec5f7e382a5ae5d7de183
Shaheen <mhshaheen.shadhin@gmail.com>
2023-09-25 16:03:19 +0600

    chat screen layout

---
+++ app/src/main/java/com/example/firestorechatapp/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.firestorechatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firestorechatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
