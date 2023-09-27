4f0f34b75269d0530745f6111bfa11b976ebff9e
Shaheen <mhshaheen.shadhin@gmail.com>
2023-09-27 16:23:59 +0600

    push notification

--- app/src/main/java/com/example/firestorechatapp/utils/Constants.java
+++ app/src/main/java/com/example/firestorechatapp/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.example.firestorechatapp.utils;
 
+import java.util.HashMap;
+
 public class Constants {
 
     public static final String KEY_COLLECTION_USERS = "users";
@@ -23,6 +25,29 @@ public class Constants {
     public static final String KEY_SENDER_IMAGE = "senderImage";
     public static final String KEY_RECEIVER_IMAGE = "receiverImage";
     public static final String KEY_LAST_MESSAGE = "lastMessage";
-
     public static final String KEY_AVAILABILITY = "availability";
+
+    // Below string are must same...........
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> remoteMsgHeaders = null;
+
+    public static HashMap<String, String> getRemoteMsgHeaders() {
+        if (remoteMsgHeaders == null) {
+            remoteMsgHeaders = new HashMap<>();
+
+            remoteMsgHeaders.put(
+                    Constants.REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAAyGF1V-w:APA91bEjn5SpUg3GFtE-qtIJX5r4e0Ch7gADLtF56Kf8LanZumd7v81kM8_cK11zRN0FqTfmMqcs6Ajpq3PfSBeO6D4VtzWoOZEdUdQUTE9RE_aHZzy_kk_2twqRHhJJhp6YsPozgjIy"
+            );
+            remoteMsgHeaders.put(
+                    Constants.REMOTE_MSG_CONTENT_TYPE,
+                    "application/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
